8845a0a820ae8efcc3858bd98bb2555bcaf02577
Dastan_Maratov <Dastan_Maratov@epam.com>
2023-04-02 13:22:05 +0600

    test

--- product-service/lambdas/getProductsList.js
+++ product-service/lambdas/getProductsList.js
@@ -4,6 +4,10 @@ import { products } from "../products.js";
 export const handler = async (event) => {
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
     body: JSON.stringify(products),
   };
 };
